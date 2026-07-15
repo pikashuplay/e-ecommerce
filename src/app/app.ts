def6bafc167d8ddc2f21a15f9e728f2217d62059
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-15 17:24:57 -0300

    refactor(features): reorganiza estrutura dos componentes de produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //!import { RouterOutlet } from '@angular/router'; //Remove RouterOutlet pois não estou usando ele no momento
-import { Produto } from './components/produto/produto'; //importa o componente para produto
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import { Produto } from './features/produtos/produto/produto'; //importa o componente para produto
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
