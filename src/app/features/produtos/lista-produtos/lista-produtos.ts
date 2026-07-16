00140bf5a494ec069e0f71d69f5a64a5ef31643a
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-16 17:05:57 -0300

    feat: adiciona cálculos reativos e substituição da lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -37,5 +39,15 @@ export class ListaProdutos {
       ...listaAtual, {nome: 'Sony Playstation 5', preco:20000}
     ]);
   }
+  totalProdutos = computed(() => this.produtos().length);
+  
+  valorTotal  = computed(() => {return this.produtos().reduce((total, item) => total + item.preco,0)});
+  
+  substituirprodutos (){
+    this.produtos.set([
+      {nome: 'Arroz Fazenda', preco: 200},
+    ]);
+  }
 }
-//chore no signal
+//criamos uma totalprodutos para calcular o total de produtos
+//adicionado valortotal para somar todos os valores da lista que forem adicionados
