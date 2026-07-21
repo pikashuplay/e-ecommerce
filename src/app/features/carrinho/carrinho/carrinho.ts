44abbb7119737af85cf1c2275ce7299816c4a667
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-21 13:52:22 -0300

    feat(app): adiciona nova pasta e nova funçãoao caminho Carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //logica do carrinho virá em breve 
+  // por ora, apenas exibir a página de carrinho
+}
