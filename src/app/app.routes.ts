e977d3668109b13af3b63f490e5591db17bfa200
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-21 13:45:31 -0300

    feat(app):adiciona nova rota de navegação que leva ao carrinho do e-ecommerce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListaProdutos,
+    },
+    {
+        path: 'carrinho',
+        component: Carrinho,
+    },
+];
