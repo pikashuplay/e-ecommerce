6406b6050b2a5e3aea46678200cdee3a262f667f
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-21 13:46:35 -0300

    feat(app): adiciona novo import router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router'; //Remove RouterOutlet pois não estou usando ele no momento
-import { Produto } from './features/produtos/produto/produto'; //importa o componente para produto
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-ecommerce');
+  nomeLoja = 'mead in china'; //nome do e-ecommerce
 }
