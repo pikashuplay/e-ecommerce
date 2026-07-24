335cce42dd671690f96a4022b4577fa1dc45f22d
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-07-24 17:02:28 -0300

    feat: import de withInterceptors e httpInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(),
+  withInterceptors([httpInterceptor])),
   ]
 };
