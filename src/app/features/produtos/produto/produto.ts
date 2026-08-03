8cdd2541c52dfc4757a6cfd379da9eda34a8eaa2
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-03 16:05:52 -0300

    feat: implementar evento de adição de produtos ao carrinho

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -19,4 +19,12 @@ export class Produto {
   selecionarProduto() {
     this.produtoSelecionado.emit(this.nome);
   }
+  @Output() produtoAdicionado = new EventEmitter<{
+    nome: string;
+    preco: number;
+  }>();
+
+  adicionarAoCarrinho() {
+    this.produtoAdicionado.emit(({nome: this.nome, preco: this.preco}))
+  }
 }
