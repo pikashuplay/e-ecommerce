310d2aadf9b688ecbcc3994e583c90788c5ac6ce
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-04 17:22:49 -0300

    feat: import de função

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,17 @@
 import { Component, signal } from '@angular/core';
-//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink} from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
-
+import { Header } from './shared/layout/header/header';
+import { MatAnchor } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header, MatAnchor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-ecommerce');
-  nomeLoja = 'mead in china'; //nome do e-ecommerce
+  // nomeLoja = 'mead in china'; //nome do e-ecommerce
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
