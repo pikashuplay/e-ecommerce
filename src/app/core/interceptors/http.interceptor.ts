1783f2f880e280a9efa23bfdb51c6fd37725f27c
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-11 16:56:28 -0300

    feat: adicionar token de autenticação ao interceptor

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -3,17 +3,32 @@ import { error } from "console";
 import { tap } from "rxjs";
 import { catchError } from "rxjs";
 import { throwError } from "rxjs";
+import { inject, Inject } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+
+
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
 
-    //!TOKEN
-    const token = 'fake-jwt-token';
-    const novaReq = req.clone({
-        setHeaders: {
-        Authorization: `Bearer $ {token}`,
+    
+    const authService = inject(AuthService);
+
+    //!NOVO METODO TOKEN
+    const token = authService.obterToken();
+    //! requisição de log 
+    console.log('Requisição: ', req.url); 
+    //!token
+    
+    const novaReq = token ?
+    
+    req.clone({
+        setHeaders:{
+            Authorization: 'bearer ${token}'
         },
-    });
-    console.log('Interceptando requisição: ', req.url); 
+    }):req;
+
+    //! NOVA REQUISIÇÃO + RESPOSTA DE LOG
+    
     return next(novaReq).pipe(
         tap({
             next: (event) => console.log('RESPONDE: ', event),
