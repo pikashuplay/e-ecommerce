88a2670451e4469673954f5f8c057927c0221b57
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-11 14:01:26 -0300

    feat: imports e nova função para login e usuario

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,14 @@
 import { CanActivateFn } from "@angular/router";
-import { usuarioLogado } from "./auth";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./services/auth.service";
 
 export const authGuard: CanActivateFn = () => {
-    return usuarioLogado();
-};
+    const authservice =inject (AuthService);
+    const router = inject(Router);
+
+    if(authservice.usuarioLogado()){
+        return true;
+    }
+    return router.createUrlTree(['/login']);
+}
