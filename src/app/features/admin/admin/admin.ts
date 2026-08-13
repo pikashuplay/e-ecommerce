dd24420257acddd98cfcf71b6b35385efe2f1d8a
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-13 17:20:24 -0300

    test: teste de correção

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { computed } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal(3);
+  usuariosCadastrados = signal (8);
+
+  usuarioAtual = this.authService.usuarioAtual;
+
+  mensagemPerfil = computed(() => {
+    const usuario = this.usuarioAtual();
+    if (!usuario){
+      return('Nenhum Usuário Autenticado!')
+    }
+    return `Usuário Autenticado como: ${usuario.perfil}`;
+  });
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
