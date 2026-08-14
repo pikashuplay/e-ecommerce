905924b4a6b9cd38b4dcd93c78342ce0ecbfc715
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-14 16:00:51 -0300

    test: para butão

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,15 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { MatAnchor } from "@angular/material/button";
 
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatAnchor],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
   private authService = inject(AuthService); //! TESTE
+  private router = inject(Router)
+
+
+
   sair = this.authService.logout();
 }
