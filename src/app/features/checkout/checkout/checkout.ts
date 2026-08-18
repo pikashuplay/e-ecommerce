f7c295a8b82dcef8325e0acc0f877042d45caa34
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-18 14:12:17 -0300

    style: alteração de service para facade e importação de facade

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -6,7 +6,7 @@ import { FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { AbstractControl } from '@angular/forms';
 import { ValidationErrors } from '@angular/forms';
-import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
 import { signal } from '@angular/core';
 
 @Component({
@@ -18,7 +18,7 @@ import { signal } from '@angular/core';
 export class Checkout {
 
 
-  carrinhoService = inject(CarrinhoService);
+  carrinhoFacade = inject(CarrinhoFacade);
 
   formulario = new FormGroup ({
     nome: new FormControl ('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
@@ -28,7 +28,7 @@ export class Checkout {
 
   finalizar () {
     this.compraFinalizada.set(false);
-    if(this.carrinhoService.carrinhoVazio()){
+    if(this.carrinhoFacade.carrinhoVazio()){
       console.log('Não é posssivel finalizar a comprar com o  carrinho vazio!')
       return;
     }
@@ -40,15 +40,15 @@ export class Checkout {
 
 
     const dados = this.formulario.value;
-    const itens = this.carrinhoService.itens();
-    const total = this.carrinhoService.totalItens();
+    const itens = this.carrinhoFacade.itensCarrinho();
+    const total = this.carrinhoFacade.totalCarrinho();
     
     console.log('Compra finalizada com sucesso');
     console.log('Dados do Formulário:', dados);
     console.log('Itens do carrinho:', itens);
     console.log('Total de compras:' , total);
 
-    this.carrinhoService.limpar();
+    this.carrinhoFacade.limparCarrinho();
     this.formulario.reset();
     this.compraFinalizada.set(true);
   }
