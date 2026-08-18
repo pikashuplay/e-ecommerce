70562fd8fcf616cade31b052f930b186e4fdbe5a
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-18 14:02:03 -0300

    style: alteração de service para nova funçãoFacade

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,18 +1,17 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { CanActivateFn } from "@angular/router";
-import { AuthService } from "./services/auth.service";
-
+import { AuthFacade } from "./facades/auth.facade";
 export const adminGuard: CanActivateFn = () => {
     const router = inject(Router)
-    const authService = inject(AuthService)
+    const authFacade = inject(AuthFacade)
 
     //! 1 verificar se o usuario esta logado
-    if(!authService.estaLogado()) {
+    if(!authFacade.usuarioLogado()) {
         return router.createUrlTree(['/login'])
     }
     //! 2 verificar se o usuario atual (Logado), se ele tem perfil adm
-    if(!authService.Admin()) {
+    if(!authFacade.admin()) {
         return router.createUrlTree(['/acesso-negado'])
     }
     //! 3 se o usuario estiver logado e for adm = ACESSO LIBERADO
