03f1b9586100ed4ad425a59356dcf0580ba99b08
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-18 17:09:45 -0300

    feat: import

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -3,6 +3,7 @@ import { UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardContent, MatCardHeader, MatCardActions, MatCardTitle} from "@angular/material/card";
+import { ItemCarrinho } from '../../../core/models/item-carrinho';
 @Component({
   selector: 'app-produto',
   imports: [UpperCasePipe, PrecoFormatadoPipe, MatButton, MatCard, MatCardTitle, MatCardContent, MatCardHeader, MatCardActions],
@@ -21,10 +22,7 @@ export class Produto {
   selecionarProduto() {
     this.produtoSelecionado.emit(this.nome);
   }
-  @Output() produtoAdicionado = new EventEmitter<{
-    nome: string;
-    preco: number;
-  }>();
+  @Output() produtoAdicionado = new EventEmitter<ItemCarrinho>();
 
   adicionarAoCarrinho() {
     this.produtoAdicionado.emit(({nome: this.nome, preco: this.preco}))
