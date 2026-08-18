f6695f4fdd9708cf226797cc6c21b6659c44477c
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-18 17:05:40 -0300

    feat(carrinho): implementar remoção individual de itens

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { ɵInternalFormsSharedModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe, ɵInternalFormsSharedModule],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
-  //logica do carrinho virá em breve 
-  // por ora, apenas exibir a página de carrinho
+  public carrinhoFacade  = inject (CarrinhoFacade);
+  private router = inject (Router);
+  private authFacade = inject (AuthFacade);
+
+  removerItem(rmvItem: number){
+    this.carrinhoFacade.limparCarrinho();
+  }
+
+
+  limparCarrinho(){
+    this.carrinhoFacade.limparCarrinho();
+  }
+
+  cancelarCompra(){
+    this.authFacade.sair();
+    this.carrinhoFacade.limparCarrinho();                                                                                          
+    this.router.navigateByUrl('/login');
+  }
 }
