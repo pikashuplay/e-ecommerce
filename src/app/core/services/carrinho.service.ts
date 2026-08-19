c59f34843e7710f6158292f90146fd892b04ad71
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-19 16:06:20 -0300

    feat(carrinho): implementar persistência no localStorage

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, signal } from "@angular/core";
-import { Signal } from "@angular/core";
+import { inject, effect } from "@angular/core";
 import { computed } from "@angular/core";
 import { ItemCarrinho } from "../models/item-carrinho";
+import { PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 
 @Injectable({
@@ -9,8 +11,16 @@ import { ItemCarrinho } from "../models/item-carrinho";
 })
 
 export class CarrinhoService {
+
+
+    private platformId = inject(PLATFORM_ID);
+
+    //!chave de recuperação localstorage
+
+    private readonly chaveStorage = 'carrinho-storage';
+
     //! estado global
-    private carrinho = signal<ItemCarrinho[]>([]);
+    private carrinho = signal<ItemCarrinho[]>(this.carregarcarrinhoSalvo());
 
     //?seletores
     itens = computed(()=> this.carrinho());
@@ -21,7 +31,43 @@ export class CarrinhoService {
 
     carrinhoVazio = computed(() => this.carrinho().length === 0);
 
+    //! ======= PERSISTENCIA CARRINHO ========
 
+    constructor(){
+        effect(() => {
+            this.SalvarCarrinho(this.carrinho());
+        });
+    }
+
+    private estaNoNavegador (): boolean{
+        return isPlatformBrowser(this.platformId);
+    }
+
+    private carregarcarrinhoSalvo(): ItemCarrinho [] {
+        if(!this.estaNoNavegador()){
+            return[];
+        }
+
+        const dadosSalvos = localStorage.getItem(this.chaveStorage);
+
+        if(!dadosSalvos){
+            return [];
+        }
+
+        try {
+            return JSON.parse(dadosSalvos) as ItemCarrinho [];
+        }catch{
+            return[];
+        }
+    }
+
+    private SalvarCarrinho (item: ItemCarrinho []){
+        if(!this.estaNoNavegador()){
+            return;
+        }
+        localStorage.setItem(this.chaveStorage, JSON.stringify(item));
+
+    }
 
     // TODO: Ações
     adicionar(produto:ItemCarrinho){
