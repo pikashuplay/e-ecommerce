3ee9193c7784f6ab1d485a80296f92a04fb07ac3
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-19 16:04:14 -0300

    feat: criação de pasta

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { CanActivateFn } from "@angular/router";
+import { AuthFacade } from "../facades/auth.facade";
+
+
+export const adminGuard: CanActivateFn = () => {
+    const router = inject(Router)
+    const authFacade = inject(AuthFacade)
+
+    //! 1 verificar se o usuario esta logado
+    if(!authFacade.usuarioLogado()) {
+        return router.createUrlTree(['/login'])
+    }
+    //! 2 verificar se o usuario atual (Logado), se ele tem perfil adm
+    if(!authFacade.admin()) {
+        return router.createUrlTree(['/acesso-negado'])
+    }
+    //! 3 se o usuario estiver logado e for adm = ACESSO LIBERADO
+
+    return true;
+}
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthFacade } from "../facades/auth.facade";
+
+
+export const authGuard: CanActivateFn = () => {
+    const authFacade = inject (AuthFacade);
+    const router = inject(Router);
+
+    if(authFacade.usuarioLogado()){
+        return true;
+    }
+    return router.createUrlTree(['/login']);
+}
