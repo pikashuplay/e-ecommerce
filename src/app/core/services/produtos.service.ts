eb211fe9333afeaa1da6ceee3618311c62eb5e7c
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-08-21 16:57:21 -0300

    test: abc

--- src/app/core/services/produtos.service.ts
+++ src/app/core/services/produtos.service.ts
@@ -1,28 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Inject,Injectable } from "@angular/core";
 import { inject } from "@angular/core";
+import { ProdutoLoja } from "../models/produto-loja";
 
 type ProdutoApi = {
     title: string;
     price: number;
 };
-type Produto = {
-    nome: string;
-    preco: number;
-}
+
 @Injectable({providedIn: 'root'})
 export class produtoService {
     private http = inject(HttpClient);
 
-    private Api = 'https://fakestoreapi.com/products'; //!n mexe nisso seu lixo
+    private readonly API = 'https://fakestoreapi.com/products'; //!n mexe nisso seu lixo
 
     buscarProduto() {
-        return this.http.get < ProdutoApi []>(this.Api);
+        return this.http.get < ProdutoApi []>(this.API);
     }
-    transformarProdutos( dados: ProdutoApi []): Produto[] {
-        return dados.map((p) =>({
-            nome: p.title,
-            preco: p.price,
+    transformarProdutos( dados: ProdutoApi []): ProdutoLoja[] {
+        return dados.map((produto) =>({
+            nome: produto.title,
+            preco: produto.price,
         }));
 
     }
